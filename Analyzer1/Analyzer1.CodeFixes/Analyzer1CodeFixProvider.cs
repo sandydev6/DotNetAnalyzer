93811a4ee7cf2f0c6af2f8491cdfa30ee389b1f8
sandydev6 <santhanaraman.sivasankaran@outlook.com>
2023-04-05 08:37:37 +0530

    Day 1

---
+++ Analyzer1/Analyzer1.CodeFixes/Analyzer1CodeFixProvider.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SN1CustomRules
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(Analyzer1CodeFixProvider)), Shared]
+    public class Analyzer1CodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(Analyzer1Analyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            foreach (Diagnostic diagnostic in context.Diagnostics)
+            {
+                TextSpan diagnosticSpan = diagnostic.Location.SourceSpan;
+
+                switch (diagnostic.Id)
+                {
+                    case Analyzer1Analyzer.DiagnosticId:
+                        context.RegisterCodeFix(
+                          CodeAction.Create(
+                               "Insert braces",
+                               c => InsertCurlyBraces(context.Document, root.FindToken(diagnosticSpan.Start).Parent, c),
+                               "Insert braces"),
+                           diagnostic);
+                        break;
+                }
+            }
+        }
+
+        private async Task<Document> InsertCurlyBraces(Document document, SyntaxNode oldStatemnt, CancellationToken c)
+        {
+            var newStatement = oldStatemnt;
+
+            switch (oldStatemnt)
+            {
+                case IfStatementSyntax ifstate:
+                    newStatement = ifstate.WithStatement(SyntaxFactory.Block(ifstate.Statement));
+                    break;
+                case ForEachStatementSyntax foreachstate:
+                    newStatement = foreachstate.WithStatement(SyntaxFactory.Block(foreachstate.Statement));
+                    break;
+            }
+
+            newStatement = newStatement.WithAdditionalAnnotations();
+            var root = await document.GetSyntaxRootAsync(c).ConfigureAwait(false);
+            var newRoot = root.ReplaceNode(oldStatemnt, newStatement);
+            var newdoc = document.WithSyntaxRoot(newRoot);
+
+            return newdoc;
+        }
+    }
+}
---
+++ Analyzer1/Analyzer1/Analyzer1Analyzer.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+
+namespace SN1CustomRules
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class Analyzer1Analyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SN1001";//custom sn1 rule 1
+
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+
+        private static DiagnosticDescriptor SyntaxRule = new DiagnosticDescriptor(DiagnosticId, "Insert braces", "the statement {0} does not contains braces", "syntax", DiagnosticSeverity.Warning, true);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(SyntaxRule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeSyntax, SyntaxKind.IfStatement, SyntaxKind.ForEachStatement);
+        }
+
+        //diagnostic braces missing in if, for, foreach and while
+        private static void AnalyzeSyntax(SyntaxNodeAnalysisContext context)
+        {
+            var body = default(StatementSyntax);
+            var token = default(SyntaxToken);
+            switch (context.Node)
+            {
+                case IfStatementSyntax ifstate:
+                    body = ifstate.Statement;
+                    token = ifstate.IfKeyword;
+                    break;
+                case ForEachStatementSyntax foreachstate:
+                    body = foreachstate.Statement;
+                    token = foreachstate.ForEachKeyword;
+                    break;
+                case ForStatementSyntax forstate:
+                    body = forstate.Statement;
+                    token = forstate.ForKeyword;
+                    break;
+                case WhileStatementSyntax whilestate:
+                    body = whilestate.Statement;
+                    token = whilestate.WhileKeyword;
+                    break;
+            }
+
+            if (!body.IsKind(SyntaxKind.Block))
+            {
+                var diagnostic = Diagnostic.Create(SyntaxRule, token.GetLocation(), context.Node.Kind().ToString());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
